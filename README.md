d04d29f901164c5cac22652e46d90406290da695
Cat Thanh <catthanh26@gmail.com>
2022-04-09 09:39:13 +0700

    add controller pattern and update readme

--- README.md
+++ README.md
@@ -2,33 +2,37 @@
 
 ## các package sử dụng
 
--   dotenv lưu biến môi trường
--   express web framework
--   morgan log response
--   http-errors xử lý error message (404 not found, not authorized...)
--   jsonwebtoken token
--   mongoose kết nối db
--   joi form validation
--   bcrypt encode mật khẩu
+-   **dotenv** lưu biến môi trường
+-   **express** web framework
+-   **morgan** log response
+-   **http-errors** xử lý error message (404 not found, not authorized...)
+-   **jsonwebtoken** token
+-   **mongoose** kết nối db
+-   **joi** form validation
+-   **bcrypt** encode mật khẩu
 
 ## cấu trúc thư mục
 
 ```bash
 |   .env # config variable
 |   app.js # khởi chạy server
-|   package.json # thông tin pakage
+|   package.json # thông tin package
 |   README.md
 |   rest.http # test api (cài extension REST Client VSCode)
 |
 +---helpers
-|       init_mongodb.js # kết nối môngdb
+|       init_mongodb.js # kết nối mongodb
 |       jwt_helper.js # xác thực, cấp token, chặn token
 |       schema_validation.js # validate thông tin đăng kí
 |
 +---models
-|       Refresh.model.js # lưu các token đã cấp/ bị chặn
+|       Refresh.model.js # lưu các refresh token đã cấp/ bị chặn
 |       User.model.js #
 |
 \---routes
         auth.route.js # xử lý route login logout refresh token register
 ```
+
+## TODO
+
+connect to cloud database
--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const morgan = require("morgan");
+const express = require("express");
+const createError = require("http-errors");
+const AuthRoute = require("./routes/auth.route");
+const { verifyAccessToken } = require("./helpers/jwt_helper");
+require("dotenv").config();
+require("./helpers/init_mongodb");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", verifyAccessToken, async (req, res, next) => {
+    res.send("Hello World");
+});
+app.use("/auth", AuthRoute);
+app.use(async (req, res, next) => {
+    next(createError.NotFound());
+});
+app.use(async (err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            status: err.status || 500,
+            message: err.message || "Internal server error",
+        },
+    });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-module.exports = app;
