b410c95072bb6e827340ac38d7cfd006e2f9884b
Cat Thanh <catthanh26@gmail.com>
2022-04-30 15:42:57 +0700

    add swaggerUi

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
 const morgan = require("morgan");
 const express = require("express");
+const cors = require("cors");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const createError = require("http-errors");
 const AuthRoute = require("./routes/auth.route");
 const RestaurantRoute = require("./routes/Restaurant.route");
 const { verifyAccessToken } = require("./helpers/jwt_helper");
 require("dotenv").config();
-//require("./helpers/init_mongodb");
 const db = require("./models");
 const app = express();
 
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Mobile API Documentation",
+            version: "1.0.0",
+        },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+        servers: [
+            {
+                url: "https://shopee-food-mobile.herokuapp.com/",
+                description: "The production API server",
+            },
+            {
+                url: "http://localhost:3000/",
+                description: "development",
+            },
+        ],
+    },
+    apis: ["./routes/*.route.js"], // files containing annotations as above
+};
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,11 +72,11 @@ app.use(async (err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-db.sequelize.sync({ alter: false }).then((req) => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-});
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
+// db.sequelize.sync({ alter: true }).then((req) => {
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//     });
 // });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
