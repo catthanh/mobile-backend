e164f15a2341af9abc576df2e7563e2f017433e9
phuc-ueters <tranthanhphuc12a5@gmail.com>
2022-06-06 14:49:44 +0700

    add middleware

--- controllers/Restaurant.controller.js
+++ controllers/Restaurant.controller.js
@@ -2,100 +2,97 @@ const createError = require("http-errors");
 const { options } = require("joi");
 const { restaurantOwner } = require("../helpers/permission");
 
-const Restaurant = require("../models").Restaurant;
+const Restaurant = require('../models').Restaurant;
 const {
-    restaurantAddReqSchema,
-    restaurantGetReqSchema,
-    restaurantModifyReqSchema,
-    restaurantRemoveReqSchema,
-} = require("../helpers/schema_validation");
+  restaurantAddReqSchema,
+  restaurantGetReqSchema,
+  restaurantModifyReqSchema,
+  restaurantRemoveReqSchema,
+} = require('../helpers/schema_validation');
 
-const internalError = createError.InternalServerError;
+const internalError = createError.internalError;
 module.exports = {
-    /**
-     * tested
-     * phuc
-     */
-    get: async (req, res, next) => {
-        try {
-            await restaurantGetReqSchema.validateAsync(req.query);
+  /**
+   * tested
+   * phuc
+   */
+  get: async (req, res, next) => {
+    try {
+      await restaurantGetReqSchema.validateAsync(req.query);
 
-            if (req.query?.id) {
-                const restaurants = await Restaurant.findByPk(req.query?.id);
-                res.send(restaurants);
-            } else {
-                const { pageNumber = 1, pageSize = 100 } = req.query;
-                const restaurants = await Restaurant.findAndCountAll({
-                    offset: (pageNumber - 1) * pageSize,
-                    limit: pageSize * 1,
-                });
-                res.send(restaurants);
-            }
-        } catch (error) {
-            if (error.isJoi === true) next(createError.BadRequest());
-            next(internalError);
-        }
-    },
-    /**
-     * tested
-     * phuc
-     */
-    add: async (req, res, next) => {
-        try {
-            await restaurantAddReqSchema.validateAsync(req.body, {
-                allowUnknown: true,
-            });
-            const { aud: idUser } = req.payload;
-            const result = await Restaurant.create({
-                ...req.body,
-                idUser: idUser,
-            });
-            res.send(result);
-        } catch (error) {
-            if (error.isJoi === true) next(createError.BadRequest());
-            next(internalError);
-        }
-    },
-    /**
-     * tested
-     * phuc
-     */
-    modify: async (req, res, next) => {
-        try {
-            await restaurantModifyReqSchema.validateAsync(req.body, {
-                allowUnknown: true,
-            });
-            const { restaurant } = req.payload;
-            if (restaurant) {
-                restaurant.update({ ...rest });
-                res.send(restaurant);
-            } else {
-                next(createError.Unauthorized());
-            }
-        } catch (error) {
-            console.log(error);
-            if (error.isJoi === true) next(createError.BadRequest());
-            next(internalError);
-        }
-    },
-    /**
-     * tested
-     * phuc
-     */
-    remove: async (req, res, next) => {
-        try {
-            await restaurantRemoveReqSchema.validateAsync(req.body);
-            const { restaurant } = req.payload;
-            if (restaurant) {
-                restaurant.destroy();
-                res.send(restaurant);
-            } else {
-                next(createError.Unauthorized());
-            }
-        } catch (error) {
-            console.log(error);
-            if (error.isJoi === true) next(createError.BadRequest());
-            next(internalError);
-        }
-    },
+      if (req.query?.id) {
+        const restaurants = await Restaurant.findByPk(req.query?.id);
+        res.send(restaurants);
+      } else {
+        const { pageNumber = 1, pageSize = 100 } = req.query;
+        const restaurants = await Restaurant.findAndCountAll({
+          offset: (pageNumber - 1) * pageSize,
+          limit: pageSize * 1,
+        });
+        res.send(restaurants);
+      }
+    } catch (error) {
+      if (error.isJoi === true) next(createError.BadRequest());
+      next(internalError);
+    }
+  },
+  /**
+   * tested
+   * phuc
+   */
+  add: async (req, res, next) => {
+    try {
+      await restaurantAddReqSchema.validateAsync(req.body, {
+        allowUnknown: true,
+      });
+      const { aud: idUser } = req.payload;
+      const result = await Restaurant.create({ ...req.body, idUser: idUser });
+      res.send(result);
+    } catch (error) {
+      if (error.isJoi === true) next(createError.BadRequest());
+      next(internalError);
+    }
+  },
+  /**
+   * tested
+   * phuc
+   */
+  modify: async (req, res, next) => {
+    try {
+      await restaurantModifyReqSchema.validateAsync(req.body, {
+        allowUnknown: true,
+      });
+      const { restaurant } = req.payload;
+      if (restaurant) {
+        restaurant.update({ ...rest });
+        res.send(restaurant);
+      } else {
+        next(createError.Unauthorized());
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.isJoi === true) next(createError.BadRequest());
+      next(internalError);
+    }
+  },
+  /**
+   * tested
+   * phuc
+   */
+  remove: async (req, res, next) => {
+    try {
+      await restaurantRemoveReqSchema.validateAsync(req.body);
+      const { restaurant } = req.payload;
+      if (restaurant) {
+        restaurant.destroy();
+        res.send(restaurant);
+      } else {
+        next(createError.Unauthorized());
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.isJoi === true) next(createError.BadRequest());
+      next(internalError);
+    }
+  },
 };
--- routes/Restaurant.route.js
+++ routes/Restaurant.route.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const RestaurantController = require("../controllers/Restaurant.controller");
-const { restaurantOwner } = require("../helpers/permission");
-const FoodRoute = require("./Food.route");
+const RestaurantController = require('../controllers/Restaurant.controller');
+const { restaurantOwner } = require('../helpers/permission');
+const FoodRoute = require('./Food.route');
 // const CategoryRoute = require("./Category.route")
-const VoucherRoute = require("./Voucher.route")
+const VoucherRoute = require('./Voucher.route');
 /**
  * @swagger
  * tags:
@@ -22,19 +22,19 @@ const VoucherRoute = require("./Voucher.route")
  *        name: id
  *        required: false
  *        description: numeric id of a restaurant, if null return all
- *        schema: 
+ *        schema:
  *          type: integer
  *      - in: path
  *        name: pageNumber
  *        required: false
  *        default: 1
- *        schema: 
+ *        schema:
  *          type: integer
  *      - in: path
  *        name: pageSize
  *        required: false
  *        default: 100
- *        schema: 
+ *        schema:
  *          type: integer
  *     responses:
  *       200:
@@ -43,15 +43,15 @@ const VoucherRoute = require("./Voucher.route")
  *         schema:
  *          type: object
  *          properties:
- *            id: 
+ *            id:
  *             type: integer
  *            name:
  *             type: string
  *            address:
- *             type: string      
- *       		
+ *             type: string
+ *
  */
-router.get("/", RestaurantController.get);
+router.get('/', RestaurantController.get);
 /**
  * @swagger
  * /restaurant:
@@ -61,16 +61,16 @@ router.get("/", RestaurantController.get);
  *      - in: body
  *        name: name
  *        required: true
- *        schema: 
+ *        schema:
  *          type: string
  *      - in: body
  *        name: address
  *        required: true
- *        schema: 
- *          type: string   
- *       		
+ *        schema:
+ *          type: string
+ *
  */
-router.post("/", restaurantOwner, RestaurantController.add);
+router.post('/', restaurantOwner, RestaurantController.add);
 /**
  * @swagger
  * /restaurant:
@@ -80,21 +80,21 @@ router.post("/", restaurantOwner, RestaurantController.add);
  *      - in: body
  *        name: id
  *        required: true
- *        schema: 
+ *        schema:
  *          type: integer
  *      - in: body
  *        name: name
  *        required: false
- *        schema: 
- *          type: string   
+ *        schema:
+ *          type: string
  *      - in: body
  *        name: address
  *        required: false
- *        schema: 
- *          type: string   
- *       		
+ *        schema:
+ *          type: string
+ *
  */
-router.put("/", restaurantOwner, RestaurantController.modify);
+router.put('/', restaurantOwner, RestaurantController.modify);
 /**
  * @swagger
  * /restaurant:
@@ -104,13 +104,13 @@ router.put("/", restaurantOwner, RestaurantController.modify);
  *      - in: body
  *        name: id
  *        required: true
- *        schema: 
+ *        schema:
  *          type: integer
- *       		
+ *
  */
-router.delete("/", restaurantOwner, RestaurantController.remove);
+router.delete('/', restaurantOwner, RestaurantController.remove);
 
-router.use('/food', FoodRoute)
-// router.use('/category', CategoryRoute)
-router.use('/voucher', VoucherRoute)
+router.use('/food', FoodRoute);
+router.use('/category', CategoryRoute);
+router.use('/voucher', VoucherRoute);
 module.exports = router;
