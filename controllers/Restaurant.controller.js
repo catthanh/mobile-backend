fa5929f102fdf6ba1527ca91ccf88e824553bdbf
Le Duy Son <leduyson2632001@gmail.com>
2022-06-21 00:42:31 +0700

    add api get restaurants by category

--- controllers/Restaurant.controller.js
+++ controllers/Restaurant.controller.js
@@ -12,6 +12,7 @@ const Favourite = require("../models").Favourite;
 const Food = require("../models").Food;
 const Review = require("../models").Review;
 const User = require("../models").User;
+const ResFuitable = require("../models").ResFuitable;
 
 const {
     restaurantAddReqSchema,
@@ -21,7 +22,8 @@ const {
     restaurantGetByIdSchema,
     restaurantGetByDistance,
     restaurantGetByFiltered,
-    restaurantGetDetailsSchema
+    restaurantGetDetailsSchema,
+    restaurantGetByCategorySchema
 } = require("../helpers/schema_validation");
 
 const Utilizer = require("../helpers/utils");
@@ -160,6 +162,61 @@ const getRestaurantsbyId = async (req, res, next) => {
     }
 };
 
+const getRestaurantsbyCategory = async (req, res, next) => {
+  try {
+      await restaurantGetByCategorySchema.validateAsync(req.query, {
+          allowUnknown: true,
+      });
+      
+      const userId = req.payload.aud;
+      var userLoc = await Utilizer.getUserCurrentLocation(userId);
+      userLoc = [userLoc.latitude, userLoc.longitude];
+
+      const categoryId = req.query.categoryId;
+
+      const restaurantResults = await ResFuitable.findAll({
+          attributes: [],
+          where: {
+              idFuitable: categoryId,
+          },
+          include: [{
+              model: Restaurant,
+              required: true,
+              attributes: ['id', 'name', 'coverImageLink', 'address', 'avgRating', 'latitude', 'longtitude', 'preparationTime']
+          }]
+      })
+
+      restaurantResults.forEach((element, index) => {
+          element = element.Restaurant;
+          const targetLoc = [element.latitude, element.longtitude];
+          const distance = Utilizer.calDistanceByLatLong(userLoc, targetLoc);
+
+          // calculate total time to ship
+          const totalTime = Utilizer.getShippingTime(distance, element.preparationTime);
+
+          returnElement = {
+              "restaurantName": element.name,
+              "restaurantImage": element.coverImageLink,
+              "restaurantId": element.id,
+              "restaurantAddress": element.address,
+              "avgRating": element.avgRating,
+              "Distance": distance ? distance.toFixed(1) : "0",
+              "shippingTime": parseInt(totalTime),
+              "Vouchers": element.Vouchers
+          };
+
+          restaurantResults[index] = returnElement;
+      });
+
+      return restaurantResults;
+      
+  } catch (error) {
+      if (error.isJoi === true) next(createError.BadRequest());
+      console.log(error);
+      next(internalError);
+  }
+};
+
 module.exports = {
   /**
    * tested
@@ -257,6 +314,8 @@ module.exports = {
             restaurantResults = await getRestaurantbyDistance(req, res, next);
         } else if (filter == "id") {
             restaurantResults = await getRestaurantsbyId(req, res, next);
+        } else if (filter == "category") {
+            restaurantResults = await getRestaurantsbyCategory(req, res, next);
         }
 
         res.send(restaurantResults);
