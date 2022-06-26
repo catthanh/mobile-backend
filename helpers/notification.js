c0b13808975fc553015ac06c5ce8edafa7c68476
Le Duy Son <leduyson2632001@gmail.com>
2022-06-26 16:31:57 +0700

    add util for noti

--- helpers/notification.js
+++ helpers/notification.js
@@ -1,25 +1,106 @@
+const FcmToken = require("../models").FcmToken;
+const firebase = require("firebase-admin");
+
+const getUserFcmToken = async (userId) => {
+  const results = await FcmToken.findOne({
+      where: {id: userId},
+  })
+
+  return results;
+}
+
+const getMultiUserFcmToken = async (userIds) => {
+  const results = await FcmToken.findAll({
+      where: {
+        id: userIds
+      },
+  })
+
+  return results;
+}
+
 module.exports = {
-    getNotificationData: (data, firebaseToken) => {
+    getNotiSpecificDevice: (notiData, userId) => {
+        const firebaseToken = getUserFcmToken(userId);
+
         // Create title and body
-        let body = data.body;
-        let title = data.title;
+        const body = notiData.body;
+        const title = notiData.title;
       
         let notificationData = {
           message: {
             notification: {
-              title: title,
-              body: body,
+              title: title, // title of notification 
+              body: body, // describe of notification
             },
+            data: notiData.data, // payload data
             token: firebaseToken, // registration token
           },
       
           // Meta Data for logging (store in database)
-          userId: data.userId, // User info
+          userId: userId, // User info
           sent: false, // Notification status
           error: null, // Errors if present
           date: new Date(),
         };
       
         return notificationData;
-      }
-  }
+    },
+
+    getNotiMultipleDevice: (notiData, userIds) => {
+      const firebaseTokens = getMultiUserFcmToken(userIds);
+
+      // Create title and body
+      const body = notiData.body;
+      const title = notiData.title;
+    
+      let notificationData = {
+        message: {
+          notification: {
+            title: title, // title of notification 
+            body: body, // describe of notification
+          },
+          data: notiData.data, // payload data
+          tokens: firebaseTokens, // registration token
+        },
+    
+        // Meta Data for logging (store in database)
+        userIds: userIds, // User info
+        sent: false, // Notification status
+        error: null, // Errors if present
+        date: new Date(),
+      };
+    
+      return notificationData;
+    },
+
+    getNotiTopic: (notiData, topic) => {
+      const notificationData = {
+        message: {
+          data: notiData.data, // payload data
+          topic: topic // topic that we push message to
+        },
+    
+        // Meta Data for logging (store in database)
+        sent: false, // Notification status
+        error: null, // Errors if present
+        date: new Date(),
+      };
+    
+      return notificationData;
+    },
+
+    setSubscribeToTopic: async (topic, userId) => {
+      const regisTokens = getUserFcmToken(userId);
+
+      await firebase.messaging().subscribeToTopic(regisTokens, topic)
+        .then((response) => {
+          // See the MessagingTopicManagementResponse reference documentation
+          // for the contents of response.
+          console.log('Successfully subscribed to topic:', response);
+        })
+        .catch((error) => {
+          console.log('Error subscribing to topic:', error);
+        });
+    }
+}
